fa1092c217ae41facf9114fc7a633e4dc0b7ddf8
Sixten <Sixtenpaulsson@gmail.com>
2023-10-04 09:44:39 +0200

    Commit

---
+++ python.py
@@ -0,0 +1,37 @@
+#Importeringar
+#import opencv
+#
+
+#Antal loops
+#Image
+#Canvas
+
+#Lista på alla former
+
+#Mainloopen tills antal loops
+    
+    #Få ut ungefärliga färgen på resten av bilden
+    
+    #For antal omgångar
+        #Fyll en lista med 100 former
+        #Sortera dom i ordning av bäst till sämst
+        #Ta bort alla 80 sämsta
+        #Gör 5 ai evolutions av varje annan
+    
+    
+    
+    #Implementera saken
+    
+    #Image =gamla +nyform
+    
+    #Räkna ut nya Procentet
+    
+#Visa upp bilden
+
+
+
+
+
+#Funktioner
+
+#bästtill sämst
